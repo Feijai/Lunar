396dda61d90bbd0334833fb60601a1a1cc55b621
kimn <a9523023650@gmail.com>
2023-06-18 20:48:30 +0800

    Updates

---
+++ assets/index-e0bf200f.js
@@ -0,0 +1,37 @@
+import{aH as t,b2 as e,r as o,aD as i,aK as n}from"./index-a9d8d12a.js";const{Header:a,Footer:r,Content:p}=e;t(p)``;t(a)`
+  height: 70px;
+  background-color: white;
+`;t(r)`
+  background-color: gray;
+  color: white;
+`;t.section`
+  background-color: gray;
+  text-align: center;
+  padding-top: 90px;
+  height: calc(743px - 90px);
+  h1 {
+    font-size: 48px;
+    line-height: 60px;
+    font-weight: bold;
+  }
+  .textArea {
+    font-size: 24px;
+    margin-top: 21px;
+    line-height: 30px;
+  }
+  img {
+    width: 758px;
+    height: 474px;
+    background-color: green;
+    margin-top: 60px;
+  }
+`;t.section`
+  .text {
+    line-height: 24px;
+  }
+  h2 {
+    line-height: 60px;
+    font-weight: bold;
+    font-size: 36px;
+  }
+`;function h(){return o.useEffect(()=>{window.location.href="https://lunar-sigma.vercel.app/"},[]),i(n,{})}export{h as default};
