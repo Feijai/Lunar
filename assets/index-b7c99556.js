9055514cdacd08d4bff94d8499d7ab04934dfb3a
kimn <a9523023650@gmail.com>
2023-06-25 05:06:10 +0800

    Updates

--- assets/index-b7c99556.js
+++ assets/index-b7c99556.js
@@ -1,4 +1,4 @@
-import{am as t,aQ as e,r as o,ai as i,ap as a}from"./index-ac5f736c.js";const{Header:n,Footer:r,Content:p}=e;t(p)``;t(n)`
+import{am as t,aQ as e,r as o,ai as i,ap as a}from"./index-2a5c336f.js";const{Header:n,Footer:r,Content:p}=e;t(p)``;t(n)`
   height: 70px;
   background-color: white;
 `;t(r)`
